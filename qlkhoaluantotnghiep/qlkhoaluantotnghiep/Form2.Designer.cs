9e9c0fea887d9518ae889627e054e811ce5410e0
Hoang12347 <minhhoang.163tp@gmail.com>
2025-04-10 16:38:08 +0700

    nhận xét báo cáo

---
+++ qlkhoaluantotnghiep/qlkhoaluantotnghiep/Form2.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace qlkhoaluantotnghiep
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(207, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(322, 72);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "nhập kết quả hoặc điểm số ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Name = "Form2";
+            this.Text = "Nhập kết quả báo cáo ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+    }
+}
